9c33c49376a4c31bc1453f2fece0bb881ca201e5
Gebriell <gt90903@gmail.com>
2023-10-22 01:54:21 +0000

    feat: added CORS dependency

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+// Add the cors middleware
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
