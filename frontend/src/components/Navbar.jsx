9d71d58e7a0fd8e8e13cf36d4eab147c6d4f160f
igouProto <48474345+igouProto@users.noreply.github.com>
2023-11-18 17:22:17 -0800

    shortcut to homepage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='navbar'>
-        <h3 className='logo'>Secrets of Top YT Creators</h3>
+        <a href="/">
+            <h3 className='logo'>Secrets of Top YT Creators</h3>
+        </a>
         <p className='logo-sub'>Group 3</p>
     </div>
   )
