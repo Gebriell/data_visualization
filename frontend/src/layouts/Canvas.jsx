2c3cbe5236cb3acd8e8017bf2382150eb2c925e3
igouProto <48474345+igouProto@users.noreply.github.com>
2023-11-24 00:07:18 -0800

    incorporate withFooter layout

--- frontend/src/layouts/Canvas.jsx
+++ frontend/src/layouts/Canvas.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import {Route, Routes} from 'react-router-dom'
-import Question6 from '../pages/Question6'
-import Navbar from '../components/Navbar'
-import Question7 from '../pages/Question7'
-import Question7Canvas from '../pages/Question7Canvas'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Question6 from "../pages/Question6";
+import Navbar from "../components/Navbar";
+import Question7 from "../pages/Question7";
+import Question7Canvas from "../pages/Question7Canvas";
+import WithFooter from "./WithFooter";
+import Landing from "../pages/Landing";
 
 /**
  * This canvas is the main container for all the pages including the navbar
@@ -12,20 +14,37 @@ import Question7Canvas from '../pages/Question7Canvas'
 
 function Canvas() {
   return (
-    <div className='canvas'>
-        <Navbar />
+    <div className="canvas">
+      <Navbar />
 
-        <div className='content'>
-            <Routes>
-                <Route path='/' element={<h1>Home</h1>} />
-                <Route path='/q6' element={<Question6 />} />
-                <Route path='/q7' element={<Question7 />} />
-                <Route path='/q7c' element={<Question7Canvas />} />
-                <Route path='*' element={<h1>404!</h1>} />
-            </Routes>
-        </div>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route
+            path="/q6"
+            element={
+              <WithFooter
+                children={<Question6 />}
+                prev={{ link: "/", title: "Q5" }}
+                next={{ link: "/q7", title: "Q7" }}
+              />
+            }
+          />
+          <Route
+            path="/q7"
+            element={
+              <WithFooter
+                children={<Question7 />}
+                prev={{ link: "/q6", title: "Q6" }}
+                next={{ link: "", title: "" }}
+              />
+            }
+          />
+          <Route path="*" element={<h1>404!</h1>} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Canvas
+export default Canvas;
