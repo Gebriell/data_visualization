919ed971e024e72e1082b076fecc3b133ab1c137
igouProto <48474345+igouProto@users.noreply.github.com>
2023-11-24 00:08:11 -0800

    landing page stub

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div className='landing'>
+        <div className='title'>
+            <h1>Secrets of Top YT Creators</h1>
+            <p>Group 3</p>
+        </div>
+    </div>
+  )
+}
+
+export default Landing
